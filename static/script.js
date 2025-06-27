10fa6909fdd6018cb952ca6b4300b122ee1192a1
7ijfftuu <nsjjdjdjsjdn@gmail.com>
2025-06-27 16:26:32 +0530

    script.js

---
+++ static/script.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+  document.getElementById("animation").style.display = "none";
+  document.getElementById("main-content").style.display = "block";
+}, 5000); // 5 सेकंड बाद content दिखाना
